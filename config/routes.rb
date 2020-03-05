5ca40a22a2157d4f550df34eaa8dfb3e6dca3b88
okb0217 <scandal217.@gmail.com>
2020-03-05 11:49:19 +0000

    [finish]app3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books
   resources :users,only: [:show,:index,:edit,:update]
   root 'home#top'
   get 'home/about' => 'home#about'
+  resources :books do
+  	resources :book_comments, only: [:create, :destroy]
+  	resource :favorites, only: [:create, :destroy]
+  end
  end
